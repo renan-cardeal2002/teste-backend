35024939225016738b6492f260dcea965e51ee4c
renan <renan01022002@hotmail.com>
2024-12-03 15:54:07 -0300

    add validation on dto's

--- src/mensagem/dto/create-mensagem.dto.ts
+++ src/mensagem/dto/create-mensagem.dto.ts
@@ -1,6 +1,15 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateMensagemDto {
+  @IsNotEmpty()
   cliente_id: number;
+
+  @IsNotEmpty()
   numero_destino: string;
+
+  @IsNotEmpty()
   is_whatsapp: boolean;
+
+  @IsNotEmpty()
   texto: string;
 }
--- src/movimento/dto/create-movimento.dto.ts
+++ src/movimento/dto/create-movimento.dto.ts
@@ -1,8 +1,14 @@
 import { TipoMovimento } from '../enums/tipo.enum';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateMovimentoDto {
+  @IsNotEmpty()
   cliente_id: number;
+
+  @IsNotEmpty()
   tipo: TipoMovimento;
+
+  @IsNotEmpty()
   valor: number;
   observacao: string;
 }
