e67822eacee263ffbaa1263ae4858fac1b8779e0
renan <renan01022002@hotmail.com>
2024-12-04 10:30:06 -0300

    add .env to connection database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClienteModule } from './cliente/cliente.module';
 import { MensagemModule } from './mensagem/mensagem.module';
 import { SaldoModule } from './saldo/saldo.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cliente } from './cliente/entities/cliente.entity';
 import { Saldo } from './saldo/entities/saldo.entity';
 import { Mensagem } from './mensagem/entities/mensagem.entity';
@@ -13,20 +14,23 @@ import { Movimento } from './movimento/entities/movimento.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ClienteModule,
     MensagemModule,
     SaldoModule,
+    MovimentoModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5436,
-      username: 'user',
-      password: 'password',
-      database: 'database',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Cliente, Saldo, Movimento, Mensagem],
       synchronize: false,
     }),
-    MovimentoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
